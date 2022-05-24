bd489984d58c318b4297c9474d3f98bd2cd99e9a
mmiladinova <mmiladinova@users.noreply.github.com>
2022-05-24 21:50:25 +0300

    Add files via upload

---
+++ Lists as Stacks and Queues/1. Lists as Stacks and Queues - theory.py
@@ -0,0 +1,27 @@
+Stacks (list as stack) - линейна структура от данни
+Last IN, First OUT - LIFO
+empty stack => stack = []
+
+* push, insert, add
+append()
+
+* delete, pop
+pop()
+
+
+Queues
+First IN, First OUT => FIFO
+collections.deque
+* да се прави проверка дали не е празна опашката *
+
+queue = deque()
+
+* add
+append()
+
+*remove
+popleft()
+
+# Combinations:
+# append() + popleft()
+# appendleft() + pop()
