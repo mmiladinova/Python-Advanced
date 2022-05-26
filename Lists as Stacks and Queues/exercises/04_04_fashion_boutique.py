49d5531ee68fa1a09a913e17cf78460007b40ed5
mmiladinova <mmiladinova@users.noreply.github.com>
2022-05-26 09:19:59 +0300

    Add files via upload

---
+++ Lists as Stacks and Queues/exercises/04_04_fashion_boutique.py
@@ -0,0 +1,25 @@
+cloths_values_stack = list(input().split())
+rack_capacity = int(input())
+
+racks_quantity = 1
+counter = 0
+
+while cloths_values_stack:
+
+    cloth_value = int(cloths_values_stack.pop())
+    counter += cloth_value
+
+    if rack_capacity == counter and cloths_values_stack:
+        racks_quantity += 1
+        counter = 0
+    elif rack_capacity == counter and not cloths_values_stack:
+        counter = 0
+    elif counter > rack_capacity:
+        racks_quantity += 1
+        counter = cloth_value
+
+print(racks_quantity)
+
+
+
+
